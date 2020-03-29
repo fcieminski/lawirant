d8ccb7a66e81a685a4486f155eaa7f731b9e7c33
Filip Chamier-Ciemiński <f.cieminski@gmail.com>
2020-03-29 17:58:29 +0200

    working on players

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import config from "../firebase";
+import firebase from "firebase";
+
+firebase.initializeApp(config);
+firebase.analytics();
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input type="text" v-model="name" />
+    <button @click="startGame">Ok</button>
+    
+    <div v-if="createTable">
+        <button>Utwórz stół</button>
+        <button>Dołącz do stołu</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      name: '',
+      createTable: false,
+    };
+  },
+  methods: {
+    startGame() {
+      if (this.name) {
+          this.createTable = true;
+      } else {
+           this.createTable = false;
+      }
+    }
   }
 };
 </script>
