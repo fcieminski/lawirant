100efbb8a7cf4deef0a108ae542eeb0d7de73039
Filip Chamier-Ciemiński <f.cieminski@gmail.com>
2020-03-29 18:29:13 +0200

    creating table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,53 @@
 <template>
-  <div class="home">
-    <input type="text" v-model="name" />
-    <button @click="startGame">Ok</button>
-    
-    <div v-if="createTable">
-        <button>Utwórz stół</button>
-        <button>Dołącz do stołu</button>
-    </div>
-  </div>
+	<div class="home">
+		<input type="text" v-model="name" />
+		<button @click="startGame">Ok</button>
+
+		<div v-if="createTable">
+			<input placeholder="nazwa nowego stołu" type="text" v-model="tableName" />
+			<button v-show="tableName" @click="createNewTable">Utwórz stół</button>
+			<button>Dołącz do stołu</button>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Home",
-  components: {},
-  data() {
-    return {
-      name: '',
-      createTable: false,
-    };
-  },
-  methods: {
-    startGame() {
-      if (this.name) {
-          this.createTable = true;
-      } else {
-           this.createTable = false;
-      }
-    }
-  }
-};
+	import firebase from "firebase";
+	export default {
+		name: "Home",
+		components: {},
+		data() {
+			return {
+				name: "",
+				createTable: false,
+				tableName: ""
+			};
+		},
+		methods: {
+			startGame() {
+				if (this.name) {
+					firebase
+						.firestore()
+						.collection("players/")
+						.add({
+							name: this.name
+						});
+					this.createTable = true;
+				} else {
+					this.createTable = false;
+				}
+			},
+			createNewTable() {
+				if (this.tableName) {
+					firebase
+						.firestore()
+						.collection("gametable")
+						.doc("firstTable")
+						.update({
+							players: firebase.firestore.FieldValue.arrayUnion(this.name)
+						});
+				}
+			}
+		}
+	};
 </script>
