2c1e1ccc08434f64a517950445a5366c44cc2195
Filip Chamier-Ciemiński <f.cieminski@gmail.com>
2020-04-18 19:44:14 +0200

    deploy

---
+++ precache-manifest.8d1d5fe2d9bb5ddb081cc0f52c17b4c3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/cards.json"
+  },
+  {
+    "revision": "3d93185b8e03b5a9f8db",
+    "url": "/css/app.b9a06cbf.css"
+  },
+  {
+    "revision": "c259a3903dfa473389bb",
+    "url": "/css/chunk-1c6cc670.992cdcd9.css"
+  },
+  {
+    "revision": "d1b4d9d4a35340c0b352",
+    "url": "/css/chunk-b93ea0f8.6b9c467c.css"
+  },
+  {
+    "revision": "ad7a2a8a5a59e809c8d0",
+    "url": "/css/chunk-vendors.1800c649.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "c67b0c7e6b3560040758381d31319753",
+    "url": "/fonts/Voltaire-Regular.c67b0c7e.ttf"
+  },
+  {
+    "revision": "c0ea2aaa28a351d7875b4cc4eebb48ad",
+    "url": "/img/card1.c0ea2aaa.jpg"
+  },
+  {
+    "revision": "19886260c1fdc34b84c592dd12d60a6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d93185b8e03b5a9f8db",
+    "url": "/js/app.46578bda.js"
+  },
+  {
+    "revision": "c259a3903dfa473389bb",
+    "url": "/js/chunk-1c6cc670.4bae9f92.js"
+  },
+  {
+    "revision": "d1b4d9d4a35340c0b352",
+    "url": "/js/chunk-b93ea0f8.0433ae82.js"
+  },
+  {
+    "revision": "ad7a2a8a5a59e809c8d0",
+    "url": "/js/chunk-vendors.5c65855d.js"
+  },
+  {
+    "revision": "ee5aaef82606aefb48ead929059f20cf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
